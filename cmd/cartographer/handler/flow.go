5278bc88adcb20e187fa4bb1cf6d170517766edb
acollie <alex@collie.codes>
2025-10-12 14:38:36 +0100

    CHANGE: Types to be more idiomatic CHANGE: sync MAP to avoid concurrency issues

--- cmd/cartographer/handler/flow.go
+++ cmd/cartographer/handler/flow.go
@@ -5,24 +5,37 @@ import (
 	"time"
 	"webcrawler/cmd/cartographer/pkg/fetch"
 	"webcrawler/cmd/cartographer/pkg/graph"
+	"webcrawler/cmd/cartographer/pkg/metrics"
 	"webcrawler/cmd/cartographer/pkg/push"
 )
 
 func (d *Handler) Traverse() error {
 	ctx := context.Background()
+	var graphs []graph.Graph
+	sweepName := ""
 	for i := 0; i < d.sweepCount; i++ {
+		metrics.Sweeps.Inc()
 		sites := fetch.Fetch(d.db, d.sweepCount)
 		g := graph.New(sites)
+		metrics.SitesProcessed.With(map[string]string{
+			"sweepName": sweepName,
+		}).Add(float64(len(sites)))
 
 		traversed, err := graph.Traverse(ctx, g)
 		if err != nil {
 			return err
 		}
+		graphs = append(graphs, traversed)
 
-		err = push.Push(traversed, "al", time.Now())
-		if err != nil {
-			return err
-		}
+	}
+	merged, err := graph.Merge(graphs)
+	if err != nil {
+		return nil
+	}
+
+	err = push.Push(merged, sweepName, time.Now())
+	if err != nil {
+		return err
 	}
 
 	return nil
--- cmd/cartographer/pkg/metrics/site.go
+++ cmd/cartographer/pkg/metrics/site.go
@@ -1,1 +1,17 @@
 package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var (
+	SitesProcessed = promauto.NewCounterVec(prometheus.CounterOpts{
+		Name: "site_processed",
+		Help: "number of sites processed",
+	}, []string{})
+	Sweeps = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "Sweeps",
+		Help: "number of times the sweep as run",
+	})
+)
